a52debb5e14c3d808ce8b2434e23b981ee30860c
Snig27 <peter.stroissnig@student.tugraz.at>
2019-03-20 11:49:53 +0100

    CHEAT_004 editText + Testfix [SP,DP,PK]

--- app/src/androidTest/java/at/tugraz/ist/swe/cheatapp/testTextField.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/cheatapp/testTextField.java
@@ -1,13 +1,22 @@
 package at.tugraz.ist.swe.cheatapp;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
+@RunWith(AndroidJUnit4.class)
 public class testTextField {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void testFieldVisible() {
         onView(withId(R.id.textEntry)).check(matches(isDisplayed()));
--- app/src/main/java/at/tugraz/ist/swe/cheatapp/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/MainActivity.java
@@ -2,6 +2,9 @@ package at.tugraz.ist.swe.cheatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText textEntry = (EditText) findViewById(R.id.textEntry);
+        String entryString = textEntry.getText().toString();
+
+
     }
 }
