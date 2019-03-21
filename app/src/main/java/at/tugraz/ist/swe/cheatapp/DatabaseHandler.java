529ddc7c7a4fc0fdf1ae420a34f6a5d0a187947f
Elisabeth Salomon <elisabeth.salomon@student.tugraz.at>
2019-03-21 13:58:36 +0100

    CHEAT_007_a Added the DatabaseHandler class to project [ES, SE]

---
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/DatabaseHandler.java
@@ -0,0 +1,13 @@
+package at.tugraz.ist.swe.cheatapp;
+
+public class DatabaseHandler {
+
+    public DatabaseHandler(){
+
+    }
+
+    public boolean isDatabaseOpen(){
+
+        return true;
+    }
+}
