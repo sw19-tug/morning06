c3bf85c8059ba2a15a9a4f9da2eaf70935dee3a8
Philipp <philipp.diethard207@gmail.com>
2019-03-23 15:08:48 +0100

    CHEAT_004 further test for editText [DP, SP]

--- app/src/androidTest/java/at/tugraz/ist/swe/cheatapp/testTextField.java
+++ app/src/androidTest/java/at/tugraz/ist/swe/cheatapp/testTextField.java
@@ -2,15 +2,21 @@ package at.tugraz.ist.swe.cheatapp;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.EditText;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
 public class testTextField {
 
@@ -21,4 +27,13 @@ public class testTextField {
     public void testFieldVisible() {
         onView(withId(R.id.textEntry)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void testInput(){
+        String test_text = "Test_Input";
+        onView(withId(R.id.textEntry)).perform(replaceText(""));
+        onView(withId(R.id.textEntry)).perform(typeText(test_text), closeSoftKeyboard());
+        onView(withId(R.id.textEntry)).check(matches(withText(test_text)));
+    }
+
 }
