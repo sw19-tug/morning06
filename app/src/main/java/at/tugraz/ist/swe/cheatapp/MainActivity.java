5f8e8d991bc9c56d4d7144807a0e00be8125e7a7
Karl-Michael Petschl <k.petschl@student.tugraz.at>
2019-03-23 11:26:58 +0100

    CHEAT_004 refactoring of xml due to warnings in the last commit [PK,DP]

--- app/src/main/java/at/tugraz/ist/swe/cheatapp/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText textEntry = (EditText) findViewById(R.id.textEntry);
+        EditText textEntry = findViewById(R.id.textEntry);
         String entryString = textEntry.getText().toString();
-
-
     }
 }
