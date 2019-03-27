e8c5d1d240414c3779c35f422c8594421f9a8c38
Philipp <philipp.diethard207@gmail.com>
2019-03-27 11:03:28 +0100

    CHEAT_005 button tests, messages are saved in dummy device [SP, DP]

--- app/src/main/java/at/tugraz/ist/swe/cheatapp/MainActivity.java
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/MainActivity.java
@@ -1,7 +1,7 @@
 package at.tugraz.ist.swe.cheatapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,32 +10,35 @@ public class MainActivity extends AppCompatActivity {
 
     private Button sendButton;
     private EditText textEntry;
-    private DummyDevice dummy;
+    private Device device;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dummy = new DummyDevice("1");
+        device = new DummyDevice("1");
 
         textEntry = findViewById(R.id.textEntry);
 
-        sendButton=(Button) findViewById(R.id.sendButton);
+        sendButton = (Button) findViewById(R.id.sendButton);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onSendButtonclicked();
+                onSendButtonClicked();
             }
         });
 
 
     }
 
-    private void onSendButtonclicked()
-    {
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    private void onSendButtonClicked() {
         String textToSend = textEntry.getText().toString();
-        dummy.setMessage(textToSend);
+        device.setMessage(textToSend);
         textEntry.getText().clear();
     }
 }
