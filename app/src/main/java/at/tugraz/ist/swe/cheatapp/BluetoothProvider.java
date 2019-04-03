605f57d33bb97d6c07227c1c6b42378ce1b03d89
Karl-Michael Petschl <k.petschl@student.tugraz.at>
2019-04-03 09:54:12 +0200

    CHEAT_001_a BluetoothProvider: add event handler list; implement register and unregister handler; run unit tests [PK,WL]

--- app/src/main/java/at/tugraz/ist/swe/cheatapp/BluetoothProvider.java
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/BluetoothProvider.java
@@ -1,16 +1,23 @@
 package at.tugraz.ist.swe.cheatapp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BluetoothProvider {
+    protected List<BluetoothEventHandler> eventHandlerList;
+
     public abstract List<Device> getPairedDevices();
     public abstract void connectToDevice(Device device);
 
-    public void registerHandler(BluetoothEventHandler handler) {
+    public BluetoothProvider() {
+        this.eventHandlerList = new ArrayList<>();
+    }
 
+    public void registerHandler(BluetoothEventHandler handler) {
+        eventHandlerList.add(handler);
     }
 
     public void unregisterHandler(BluetoothEventHandler handler) {
-
+        eventHandlerList.remove(handler);
     }
 }
--- app/src/test/java/at/tugraz/ist/swe/cheatapp/BluetoothProviderTest.java
+++ app/src/test/java/at/tugraz/ist/swe/cheatapp/BluetoothProviderTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class BluetoothProviderTest {
     private DummyBluetoothProvider bluetoothProvider;
 
