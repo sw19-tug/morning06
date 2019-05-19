a101586f03c66b901f78f70460d12a29206ba9cc
Lukas Wagner <lwagner94@posteo.at>
2019-05-19 17:22:11 +0200

    CHEAT_002 Refactor run method in BluetoothThread

--- app/src/main/java/at/tugraz/ist/swe/cheatapp/BluetoothThread.java
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/BluetoothThread.java
@@ -20,113 +20,33 @@ public class BluetoothThread extends Thread {
     private RealBluetoothProvider provider;
     private Queue<BluetoothMessage> messageQueue;
     private boolean running;
+    private BluetoothSocket socket;
+    private BufferedReader inputReader;
+    private PrintWriter outputWriter;
 
 
     public BluetoothThread(final RealBluetoothProvider provider) {
         this.provider = provider;
         this.messageQueue = new LinkedList<>();
         this.running = true;
+        this.socket = null;
     }
 
     public synchronized void sendBluetoothMessage(final BluetoothMessage bluetoothMessage) {
         messageQueue.add(bluetoothMessage);
     }
 
-    public void setRunning(boolean running) {
+    public synchronized void setRunning(boolean running) {
         this.running = running;
     }
 
     @Override
     public void run() {
         try {
-            BluetoothSocket socket = null;
-            final BluetoothServerSocket serverSocket = provider.getAdapter().listenUsingRfcommWithServiceRecord(BLUETOOTH_SERVICE_RECORD, BLUETOOTH_UUID);
-
-            boolean runLoop = true;
-
-            // TODO: Maybe put this part into own method
-            ////////////////////////////////////////////////////////////////////////////////////
-            while (runLoop && socket == null) {
-
-                try {
-                    socket = serverSocket.accept(200);
-                    System.out.println("CommunicationThread: Connected as server.");
-                } catch (IOException timeout) {
-                    // Timeout
-                    synchronized (this) {
-                        RealDevice dev = provider.getDevice();
-                        if (dev != null) {
-                            System.out.println("CommunicationThread: Connection as client requested");
-                            try {
-                                socket = dev.getDevice().createRfcommSocketToServiceRecord(BLUETOOTH_UUID);
-                                if (socket != null) {
-                                    Log.d("RealBluetoothProvider", "before connect");
-                                    socket.connect();
-                                }
-                            } catch (IOException ex) {
-                                ex.printStackTrace();
-                                throw new RuntimeException(ex.getMessage());
-                            }
-
-                            runLoop = running;
-                        }
-
-
-                    }
-                }
-            }
-
-            serverSocket.close();
-
-            // TODO: Maybe put this part into own method
-            ////////////////////////////////////////////////////////////////////////////////////
-
-            BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter outputWriter = new PrintWriter(socket.getOutputStream());
-
-            synchronized (this) {
-                messageQueue.add(new BluetoothMessage(new ConnectMessage(BuildConfig.APPLICATION_ID, BuildConfig.VERSION_NAME)));
-            }
-            boolean disconnected = false;
-
-            while (runLoop) {
-                synchronized (this) {
-                    if (inputReader.ready()) {
-                        String receivedMessage = inputReader.readLine();
-                        final BluetoothMessage btMessage = BluetoothMessage.fromJSONString(receivedMessage);
-
-                        switch (btMessage.getMessageType()) {
-                            case CHAT:
-                                provider.onMessageReceived(btMessage.getMessage());
-                                break;
-                            case CONNECT:
-                                provider.onConnected();
-                                break;
-                            case DISCONNECT:
-                                disconnected = true;
-                                break;
-
-                        }
-                    } else {
-                        BluetoothMessage message;
-                        message = messageQueue.poll();
-
-
-                        if (message != null) {
-                            outputWriter.println(message.toJSONString());
-                            outputWriter.flush();
-                        }
-                    }
-
-                    runLoop = !messageQueue.isEmpty() || running && !disconnected;
-                }
-                Thread.sleep(100);
-            }
-
-            inputReader.close();
-            outputWriter.close();
-            socket.close();
-
+            createBluetoothSocket();
+            initializeConnection();
+            handleMessages();
+            shutdownCommunication();
             provider.onDisconnected();
 
         } catch (IOException ex) {
@@ -139,5 +59,99 @@ public class BluetoothThread extends Thread {
             ex.printStackTrace();
             throw new RuntimeException(ex);
         }
+
     }
-};
+
+    private void shutdownCommunication() throws IOException {
+        inputReader.close();
+        outputWriter.close();
+        socket.close();
+    }
+
+    private void handleMessages() throws IOException, JSONException, InterruptedException {
+        boolean disconnected = false;
+        boolean runLoop = true;
+
+        while (runLoop) {
+            synchronized (this) {
+                if (inputReader.ready()) {
+                    String receivedMessage = inputReader.readLine();
+                    final BluetoothMessage btMessage = BluetoothMessage.fromJSONString(receivedMessage);
+
+                    switch (btMessage.getMessageType()) {
+                        case CHAT:
+                            provider.onMessageReceived(btMessage.getMessage());
+                            break;
+                        case CONNECT:
+                            provider.onConnected();
+                            break;
+                        case DISCONNECT:
+                            disconnected = true;
+                            break;
+
+                    }
+                } else {
+                    BluetoothMessage message;
+                    message = messageQueue.poll();
+
+
+                    if (message != null) {
+                        outputWriter.println(message.toJSONString());
+                        outputWriter.flush();
+                    }
+                }
+
+                runLoop = !messageQueue.isEmpty() || running && !disconnected;
+            }
+            Thread.sleep(100);
+        }
+    }
+
+    private void initializeConnection() throws IOException {
+        inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        outputWriter = new PrintWriter(socket.getOutputStream());
+
+        synchronized (this) {
+            messageQueue.add(new BluetoothMessage(new ConnectMessage(BuildConfig.APPLICATION_ID, BuildConfig.VERSION_NAME)));
+        }
+    }
+
+    private void createBluetoothSocket() throws IOException {
+        final BluetoothServerSocket serverSocket = provider.getAdapter().listenUsingRfcommWithServiceRecord(BLUETOOTH_SERVICE_RECORD, BLUETOOTH_UUID);
+
+        boolean loop = true;
+
+        while (loop && socket == null) {
+
+            try {
+                socket = serverSocket.accept(200);
+                Log.d("BluetoothThread", "Connected as server");
+            } catch (IOException timeout) {
+                // Timeout
+                synchronized (this) {
+                    RealDevice dev = provider.getDevice();
+                    if (dev != null) {
+                        Log.d("BluetoothThread", "Connection as client requested");
+                        try {
+                            socket = dev.getDevice().createRfcommSocketToServiceRecord(BLUETOOTH_UUID);
+                            if (socket != null) {
+                                socket.connect();
+                                Log.d("BluetoothThread", "Connected as client");
+
+                            }
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                            throw new RuntimeException(ex.getMessage());
+                        }
+
+                        loop = running;
+                    }
+
+
+                }
+            }
+        }
+
+        serverSocket.close();
+    }
+}
