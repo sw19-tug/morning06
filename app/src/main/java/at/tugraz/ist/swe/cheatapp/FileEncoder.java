dd1970a43180117fdbb8bc20bc67ecf39ffe9b85
Lukas Wagner <lwagner94@posteo.at>
2019-06-03 13:22:30 +0200

    CHEAT_020_a Add implementation for FileEncoder [PK,WL]

---
+++ app/src/main/java/at/tugraz/ist/swe/cheatapp/FileEncoder.java
@@ -0,0 +1,25 @@
+package at.tugraz.ist.swe.cheatapp;
+
+import android.util.Base64;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileEncoder {
+
+
+    private byte[] readBytesFromFile(final File file) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte data[] = new byte[(int) file.length()];
+            fileInputStream.read(data);
+            return data;
+        }
+    }
+
+
+    public String encodeBase64(final File file) throws IOException {
+        byte[] data = readBytesFromFile(file);
+        return Base64.encodeToString(data, Base64.DEFAULT);
+    }
+}
--- app/src/test/java/at/tugraz/ist/swe/cheatapp/FileEncoderTest.java
+++ app/src/test/java/at/tugraz/ist/swe/cheatapp/FileEncoderTest.java
@@ -1,6 +1,14 @@
 package at.tugraz.ist.swe.cheatapp;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 import java.io.IOException;
@@ -8,18 +16,31 @@ import java.nio.file.Files;
 import java.util.Base64;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(android.util.Base64.class)
 
 public class FileEncoderTest {
     @Test
     public void testEncoding() throws IOException {
+        PowerMockito.mockStatic(android.util.Base64.class);
+//        when(android.util.Base64.encodeToString(any<Byte[]>(), anyInt())).thenAnswer(invocation -> java.util.Base64.getEncoder().encode((byte[]) invocation.getArguments()[0]));
+//        when(Base64.decode(anyString(), anyInt())).thenAnswer(invocation -> java.util.Base64.getMimeDecoder().decode((String) invocation.getArguments()[0]));
+
         File testImage = new File("sampledata/test_img_1.png");
         assert(testImage.exists());
         byte[] fileContent = Files.readAllBytes(testImage.toPath());
 
+        String encodedFileCheck = Base64.getEncoder().encodeToString(fileContent);
+
         FileEncoder encoder = new FileEncoder();
+        when(android.util.Base64.encodeToString(any(byte[].class), anyInt())).thenReturn(encodedFileCheck);
+
         String encodedFile = encoder.encodeBase64(testImage);
 
-        String encodedFileCheck = Base64.getEncoder().encodeToString(fileContent);
         assertEquals(encodedFile, encodedFileCheck);
     }
 }
