3c67a9487b6fcbe808ffd99aab574b805a25c230
Karl-Michael Petschl <k.petschl@student.tugraz.at>
2019-06-03 15:36:01 +0200

    CHEAT_020_a add test for encode and decode string [PK,WL]

--- app/src/test/java/at/tugraz/ist/swe/cheatapp/FileEncoderTest.java
+++ app/src/test/java/at/tugraz/ist/swe/cheatapp/FileEncoderTest.java
@@ -1,5 +1,6 @@
 package at.tugraz.ist.swe.cheatapp;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -17,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Base64;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -27,8 +29,10 @@ import static org.powermock.api.mockito.PowerMockito.when;
 @PrepareForTest(android.util.Base64.class)
 
 public class FileEncoderTest {
-    @Test
-    public void testEncodeBase64() throws IOException {
+    private FileEncoder encoder;
+
+    @Before
+    public void setUp() {
         PowerMockito.mockStatic(android.util.Base64.class);
         when(android.util.Base64.encodeToString(any(byte[].class), anyInt())).thenAnswer(new Answer<String>() {
             @Override
@@ -36,27 +40,6 @@ public class FileEncoderTest {
                 return java.util.Base64.getEncoder().encodeToString((byte[]) invocation.getArgument(0));
             }
         });
-        File testImage = new File("sampledata/test_img_1.png");
-        assert(testImage.exists());
-        byte[] fileContent = Files.readAllBytes(testImage.toPath());
-
-        /*
-        File testEncodedFile = new File("sampledata/test_img_1_encoded.txt");
-        assert (testEncodedFile.exists());
-        byte[] fileContentEncoded = Files.readAllBytes(testEncodedFile.toPath());
-        byte[] fileContentDecoded = Base64.getDecoder().decode(fileContentEncoded);
-
-        assertEquals(fileContent, fileContentDecoded); */
-
-        FileEncoder encoder = new FileEncoder();
-        String encodedFile = encoder.encodeBase64(testImage);
-        String encodedFileCheck = Base64.getEncoder().encodeToString(fileContent);
-        assertEquals(encodedFile, encodedFileCheck);
-    }
-
-    @Test
-    public void testDecodeBase64() throws IOException {
-        PowerMockito.mockStatic(android.util.Base64.class);
         when(android.util.Base64.decode(any(String.class), anyInt())).thenAnswer(new Answer<byte[]>() {
             @Override
             public byte[] answer(InvocationOnMock invocation) throws Throwable {
@@ -64,14 +47,49 @@ public class FileEncoderTest {
             }
         });
 
+        encoder = new FileEncoder();
+    }
+
+    @Test
+    public void testEncodeBase64() throws IOException {
+        File testImage = new File("sampledata/test_img_1.png");
+        assert(testImage.exists());
+        byte[] fileContent = Files.readAllBytes(testImage.toPath());
+
+        String encodedFile = encoder.encodeBase64(testImage);
+        String encodedFileCheck = Base64.getEncoder().encodeToString(fileContent);
+        assertEquals(encodedFile, encodedFileCheck);
+    }
+
+    @Test
+    public void testDecodeBase64() throws IOException {
         File testImageEncoded = new File("sampledata/test_img_1_encoded.txt");
         assert(testImageEncoded.exists());
         byte[] encodedBytes = Files.readAllBytes(testImageEncoded.toPath());
         String encodedString = new String(encodedBytes);
 
-        FileEncoder encoder = new FileEncoder();
-        byte[] decodedFile = encoder.decodeBase64(testImageEncoded);
+        byte[] decodedFile = encoder.decodeBase64(encodedString);
         byte[] decodedFileCheck = Base64.getDecoder().decode(encodedString);
-        assertEquals(decodedFile, decodedFileCheck);
+        assert(Arrays.equals(decodedFile, decodedFileCheck));
+    }
+
+
+    @Test
+    public void testEncodeDecode() throws IOException {
+        byte[] originalByte = {'a', 'b', 'c', 'd', 'e'};
+        String originalString = "abcde";
+        assertEquals(new String(originalByte), originalString);
+
+        File originalFile = new File ("sampledata/test_encode_decode.txt");
+        assert(originalFile.exists());
+        PrintWriter writer = new PrintWriter(originalFile.getPath());
+        writer.print(originalString);
+        writer.close();
+
+        String encoderTest = encoder.encodeBase64(originalFile);
+        byte[] decoderTest = encoder.decodeBase64(encoderTest);
+
+        assert(Arrays.equals(decoderTest, originalByte));
+        assertEquals(new String(decoderTest), originalString);
     }
 }
