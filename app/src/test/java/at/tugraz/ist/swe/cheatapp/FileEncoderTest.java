7c73a118b169dd624615c5d4df324bd2fa3ce1a0
Karl-Michael Petschl <k.petschl@student.tugraz.at>
2019-06-03 14:16:14 +0200

    CHEAT_020_a add test for file decoding [WL,PK]

--- app/src/test/java/at/tugraz/ist/swe/cheatapp/FileEncoderTest.java
+++ app/src/test/java/at/tugraz/ist/swe/cheatapp/FileEncoderTest.java
@@ -1,25 +1,32 @@
 package at.tugraz.ist.swe.cheatapp;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.Base64;
 
-import static junit.framework.TestCase.assertEquals;
 
 public class FileEncoderTest {
     @Test
     public void testEncoding() throws IOException {
+
+    }
+
+    @Test
+    public void testDecoding() throws IOException {
         File testImage = new File("sampledata/test_img_1.png");
         assert(testImage.exists());
         byte[] fileContent = Files.readAllBytes(testImage.toPath());
 
-        FileEncoder encoder = new FileEncoder();
-        String encodedFile = encoder.encodeBase64(testImage);
+        File testEncodedFile = new File("sampledata/test_img_1_encoded.txt");
+        assert(testEncodedFile.exists());
+        byte[] fileContentEncoded = Files.readAllBytes(testEncodedFile.toPath());
+        byte[] fileContentDecoded = Base64.getDecoder().decode(fileContentEncoded);
 
-        String encodedFileCheck = Base64.getEncoder().encodeToString(fileContent);
-        assertEquals(encodedFile, encodedFileCheck);
+        assertEquals(fileContent, fileContentDecoded);
     }
 }
